2c3c4b4a3c3a7225eb71c582d37c49f1ee03fe3c
jeromemartin <bitbucket@delaur.net>
2021-12-04 13:27:20 +0100

    Fixed command

--- plugins/modules/centreon_command.py
+++ plugins/modules/centreon_command.py
@@ -75,7 +75,6 @@ else:
 
 
 def main():
-
     module = AnsibleModule(
         argument_spec=dict(
             url=dict(required=True),
@@ -109,7 +108,7 @@ def main():
     comment = module.params["comment"]
     applycfg = module.params["applycfg"]
     state = module.params["state"]
-    validate_certs  = module.params["validate_certs"]
+    validate_certs = module.params["validate_certs"]
 
     has_changed = False
 
@@ -117,69 +116,82 @@ def main():
         centreon = Centreon(url, username, password, check_ssl=validate_certs)
     except Exception as e:
         module.fail_json(
-            msg="Unable to connect to Centreon API: %s" % e.message
+            msg="Unable to connect to Centreon API: %s" % e
         )
+        return
 
     try:
         st, poller = centreon.pollers.get(instance)
     except Exception as e:
-        module.fail_json(msg="Unable to get pollers: {}".format(e.message))
+        module.fail_json(msg="Unable to get pollers: {}".format(e))
+        return
 
     if not st and poller is None:
         module.fail_json(msg="Poller '%s' does not exists" % instance)
     elif not st:
         module.fail_json(msg="Unable to get poller list %s " % poller)
 
+    data = []
     cmd_state, cmd = centreon.commands.get(name)
 
-    if centreon.commands.exist(name) and state == "absent":
-        centreon.command.delete(name)
+    if not cmd_state and state == "present":
+        try:
+            data.append(f"Add '{name}'  '{type}'")
+
+            cmd_state, res = centreon.commands.add(name, type, line)
+            if not cmd_state:
+                module.fail_json(msg=f"Unable to create command {name} : {res}", changed=has_changed)
+                return
+
+            # Apply the host templates for create associate services
+            cmd_state, cmd = centreon.commands.get(name)
+            has_changed = True
+            data.append(f"Added command: {name} type {type}")
+        except Exception as e:
+            module.exit_json(msg=str(e))
+
+    if not cmd_state:
+        module.fail_json(msg=f"Unable to find command {name}: {data}", changed=has_changed)
+        return
+
+    if cmd_state and state == "absent":
+        centreon.commands.delete(name)
         has_changed = True
         if applycfg:
             centreon.pollers.applycfg(instance)
         module.exit_json(
             changed=has_changed, result="Command %s deleted" % name)
-
-    if not centreon.commands.exist(name) and state == "present":
-        try:
-            centreon.commands.add(
-                name,
-                type,
-                line
-            )
-        except Exception as e:
-            module.exit_json(msg='%s' % e.message)
+        return
 
     try:
-        command = centreon.commands.get(name)
-        if type != command.type:
-            command.setparam('type', type)
+        if type != cmd.type:
+            cmd.setparam('type', type)
             has_changed = True
 
-        if line != command.line:
-            command.setparam('line', line)
+        if line != cmd.line:
+            cmd.setparam('line', line)
             has_changed = True
 
         if graph:
-            command.setparam('graph', graph)
+            cmd.setparam('graph', graph)
             has_changed = True
 
         if example:
-            command.setparam('example', example)
+            cmd.setparam('example', example)
             has_changed = True
 
         if comment:
-            command.setparam('comment', comment)
+            cmd.setparam('comment', comment)
             has_changed = True
     except Exception as e:
-        module.fail_json(msg='%s' % e.message)
+        module.fail_json(msg=str(e))
 
     try:
         if applycfg:
             centreon.pollers.applycfg(instance)
             has_changed = True
     except Exception as exc:
-        module.fail_json(msg='%s' % exc.message)
+        module.fail_json(msg=str(exc))
 
     module.exit_json(changed=has_changed, msg=data)
 
